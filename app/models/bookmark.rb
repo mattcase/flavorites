99b862fae3b87d0c9fb335e9e7e35c36dbf281f7
Matt Case <matt.case@gmail.com>
2012-09-16 16:59:14 -0500

    Validation on url, name fields

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,5 @@
 class Bookmark < ActiveRecord::Base
   attr_accessible :name, :saved, :url
+  validates :url, presence: true, length: { minimum: 10 }
+  validates :name, presence: true, length: { maximum: 100 }
 end
