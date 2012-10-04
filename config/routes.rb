287afef0a217bd91ec6d49b965f59fb256224c46
Matt Case <matt.case@gmail.com>
2012-10-04 13:58:12 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Flavorites::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
+  
+  match '/help',          to: 'static_pages#help'
+  match '/about',         to: 'static_pages#about'
+  match '/contact',       to: 'static_pages#contact'
 
-  get "static_pages/about"
+  match '/new',           to: 'bookmarks#new'
+  match '/view',          to: 'bookmarks#index'
 
   resources :bookmarks
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,28 +5,47 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the h1 'Flavorites'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Flavorites')
     end
 
     it "should have the title 'Flavorites'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Flavorites | Home")
+      visit root_path
+      page.should have_selector('title', :text => "Flavorites App")
+    end
+
+    it "should not have a custom page title" do
+      visit root_path
+      page.should_not have_selector('title', text: '| Home')
     end
   end
+
 
   describe "About page" do
 
     it "should have the h1 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
-                    :text => "Flavorites | About Us")
+                    :text => "Flavorites App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    :text => "Flavorites App | Contact")
     end
   end
 end
